668031b58de68462302f8096c5a5cb987a897627
mjbogusz <mjbogusz+github@gmail.com>
2018-08-13 19:51:08 +0200

    firmware: spaces->tabs

--- firmware_stm32/main.cpp
+++ firmware_stm32/main.cpp
@@ -1,48 +1,48 @@
 #include "mbed.h"
 
 AnalogIn analogIn[4] = {
-    AnalogIn(A0),
-    AnalogIn(A1),
-    AnalogIn(A2),
-    AnalogIn(A3)
+	AnalogIn(A0),
+	AnalogIn(A1),
+	AnalogIn(A2),
+	AnalogIn(A3)
 };
 
 DigitalOut led(LED2);
 DigitalOut digitalOut[4] = {
-    DigitalOut(D0),
-    DigitalOut(D1),
-    DigitalOut(D2),
-    DigitalOut(D3)
+	DigitalOut(D0),
+	DigitalOut(D1),
+	DigitalOut(D2),
+	DigitalOut(D3)
 };
 
 Serial serial(SERIAL_TX, SERIAL_RX);
 
 int main() {
-    float value = 0;
+	float value = 0;
 
-    serial.baud(115200);
+	serial.baud(115200);
 
-    while(1) {
-        for (int i = 0; i < 4; ++i) {
-            digitalOut[0] = 0;
-            digitalOut[1] = 0;
-            digitalOut[2] = 0;
-            digitalOut[3] = 0;
-            digitalOut[i] = 1;
+	while(1) {
+		for (int i = 0; i < 4; ++i) {
+			digitalOut[0] = 0;
+			digitalOut[1] = 0;
+			digitalOut[2] = 0;
+			digitalOut[3] = 0;
+			digitalOut[i] = 1;
 
-            for (int j = 0; j < 4; ++j) {
-                value = analogIn[j].read() * 3300;
-                serial.printf("%.0f", value);
-                if (j < 3) {
-                    serial.printf(", ");
-                }
-            }
-            serial.printf("; ");
-            wait(0.001);
-        }
+			for (int j = 0; j < 4; ++j) {
+				value = analogIn[j].read() * 3300;
+				serial.printf("%.0f", value);
+				if (j < 3) {
+					serial.printf(", ");
+				}
+			}
+			serial.printf("; ");
+			wait(0.001);
+		}
 
-        serial.printf("\n");
-        led = !led;
-        wait(0.1);
-    }
+		serial.printf("\n");
+		led = !led;
+		wait(0.1);
+	}
 }
